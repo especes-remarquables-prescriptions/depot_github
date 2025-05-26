07aa3158b03395759f1ce495ade264d6c51fbfb7
KRESSMANN Matteo <matteo.kressmann@onf.fr>
2025-05-26 13:31:44 +0200

    Mise ├á jour de l'application

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd # Bibliothèque pour manipuler des données tabulaires
 import geopandas as gpd
 import folium
 from streamlit_folium import st_folium
-import streamlit.components.v1 import HTML
+from streamlit.components.v1 import HTML
 
 # --------------------- FONCTIONS ---------------------
 
@@ -694,4 +694,3 @@ if st.session_state.authenticated:
         st.markdown("### Référentiel espèces remarquables")
         iframe_code = """<iframe width="402" height="346" frameborder="0" scrolling="no" src="https://officenationaldesforets-my.sharepoint.com/personal/matteo_kressmann_onf_fr/_layouts/15/Doc.aspx?sourcedoc={a5ba8dd8-f5bc-48ff-b60c-93625e6ec7aa}&action=embedview&wdAllowInteractivity=False&wdHideGridlines=True&wdHideHeaders=True&wdDownloadButton=True&wdInConfigurator=True&wdInConfigurator=True"></iframe>"""
         html(iframe_code, height=600)
-        
